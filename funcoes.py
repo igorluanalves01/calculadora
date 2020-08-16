f0507de0c20c6f7bffccb23ce516afe32a59aafd
Igor Luan <igorluanalves01@gmail.com>
2020-08-15 21:48:41 -0300

    Funções básicas de calculadora

---
+++ funcoes.py
@@ -0,0 +1,24 @@
+
+## --------------------------------------------------
+# Onde definiremos as funções e as implementaremos
+def soma(valorx, valora):
+    print('chamando função de somar')
+    return valorx + valora
+
+
+def subtrair(valorv, valorh):
+    print('chamando função de substrair')
+    # print(50 - 20)
+    return valorv - valorh
+
+def multiplicacao(valory, valorx):
+    print( ' chamando função multiplicacão)')
+    return valory * valorx
+
+def divisao(valorm, valorw):
+    print(' chamando função divisao')
+    if valorw > 0:
+        return valorm / valorw
+    return 'nao e possivel dividir por 0'
+
+
--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-def somar(a, b):
-    print("Somando {} + {}".format(a, b))
-    resultado = a + b
-    print("Resultado: {}".format(resultado))
-    return resultado
-
+from funcoes import soma, subtrair, multiplicacao, divisao
+## --------------------------------------------------
 if __name__ == '__main__':
-    somar(10, 20)
+    # Onde chama as funções
+    print(soma(10, 20))
+
+    print(subtrair(50, 40))
+
+    print(subtrair(200, 100))
+
+    print(multiplicacao(4, 4))
+
+    print(divisao(16, 0))
+
+
+
+"""
++ soma
+- subtrai
+* multiplica
+/ divide
+a e b
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+"""
