fe7ff0fd1842f46a6979e634c655a1f3d2c90ad3
Igor Luan <igorluanalves01@gmail.com>
2020-08-17 15:04:25 -0300

    ativiade de rotina

---
+++ exercicio4.py
@@ -0,0 +1,18 @@
+from funcoes import soma, subtrair, multiplicacao, divisao
+## --------------------------------------------------
+def converter_numero(valor):
+    try:
+        return float(valor.replace(',', '.'))
+    except Exception as e:
+        p
+
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+media = (nota1 + nota2)/2
+
+if media == 10:
+    print('Aprovado com Distinção.')
+elif media >= 7 and media < 10:
+    print('Aprovado.')
+else:
+    print('Reprovado.')
--- main.py
+++ main.py
@@ -34,28 +34,65 @@ if __name__ == '__main__':
             peso_normal = True
         elif imc > 24.9 and imc < 29.9:
             print('sobrepeso')
-        elif imc > 30.0 and imc < 34.9:
-            print('obesidade grau1')
-        elif imc > 35.0 and imc < 39.9:
-            print('obesidade grau2')
+        elif imc >
+"""
+30.0 and imc < 34.9:
+           print('obesidade grau1')
+       elif imc > 35.0 and imc < 39.9:
+           print('obesidade grau2')
+       else:
+           print('obesidade grau3')
+
+
+       if peso_normal:
+           print('Parabens {}, vc é saudavel'.format(nome))
+
+       encerrar = input('aperte q para encerrar')
+       if encerrar == 'q':
+           continuar_executando = False
+
+   print('todas as pessoas que testaram: '+str(lista))
+
+   for item in lista:
+       print(item)
+
+       if item == 'igor':
+           print('hum seu nome é igor')
+
+if __name__ == '__main__':
+    # A entrada (input) do peso e da idade de uma pessoa
+    # 1. Faça um Programa que peça dois números e imprima o maior deles.
+    print('## Ler dois números e verificar o maior ##')
+    num1 = int(input('Primeiro numero: '))
+    num2 = int(input('Segundo numero: '))
+
+    if num1 > num2:
+        print(num1)
+    else:
+        print(num2)
+
+    print('## Ler um número e verificar se é positivo ou negativo ##')
+    try:
+        op1 = int(input('informe um valor: '))
+
+        if op1 < 0:
+            print('negativo')
+        elif op1 > 0:
+            print('positivo')
         else:
-            print('obesidade grau3')
+            print('numero neutro')
+    except Exception as e:
+        print('ERRO: '+str(e))
 
-
-        if peso_normal:
-            print('Parabens {}, vc é saudavel'.format(nome))
-
-        encerrar = input('aperte q para encerrar')
-        if encerrar == 'q':
-            continuar_executando = False
-
-    print('todas as pessoas que testaram: '+str(lista))
-
-    for item in lista:
-        print(item)
-
-        if item == 'igor':
-            print('hum seu nome é igor')
+    print('## Ler uma tecla e verificar se é homem ou mulher ##')
+    sexo = input('Digite (F)-Feminino, (M)-Masculino: ')
+    sexo = sexo.lower().strip()
+    if sexo in ['m', 'M', '(m)', '(M)', '1']:
+        print('sexo masculino')
+    elif sexo == 'f':
+        print('sexo feminino')
+    else:
+        print('sexo invalido')
 
 
 
